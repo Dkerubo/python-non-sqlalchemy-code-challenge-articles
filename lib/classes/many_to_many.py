713f51d0873ffb88ed64b65f9c12415173efe4d1
dkerubo <damariskerry@gmail.com>
2025-01-12 23:07:38 +0300

    third commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,86 +1,5 @@
-class Author:
-    def __init__(self, name):
-        if not isinstance(name, str) or len(name) == 0:
-            raise Exception("Name must be a non-empty string.")
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        raise AttributeError("Author name is immutable.")
-
-    def articles(self):
-        return [article for article in Article.articles if article.author == self]
-
-    def magazines(self):
-        return list({article.magazine for article in self.articles()})
-
-    def add_article(self, magazine, title):
-        if not isinstance(magazine, Magazine):
-            raise Exception("Invalid magazine instance.")
-        return Article(self, magazine, title)
-
-    def topic_areas(self):
-        return list({magazine.category for magazine in self.magazines()}) or None
-
-
-class Magazine:
-    magazines = []
-
-    def __init__(self, name, category):
-        if not isinstance(name, str) or not (2 <= len(name) <= 16):
-            raise Exception("Name must be a string between 2 and 16 characters.")
-        if not isinstance(category, str) or len(category) == 0:
-            raise Exception("Category must be a non-empty string.")
-        self._name = name
-        self._category = category
-        Magazine.magazines.append(self)
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str) or not (2 <= len(value) <= 16):
-            raise Exception("Name must be a string between 2 and 16 characters.")
-        self._name = value
-
-    @property
-    def category(self):
-        return self._category
-
-    @category.setter
-    def category(self, value):
-        if not isinstance(value, str) or len(value) == 0:
-            raise Exception("Category must be a non-empty string.")
-        self._category = value
-
-    def articles(self):
-        return [article for article in Article.articles if article.magazine == self]
-
-    def contributors(self):
-        return list({article.author for article in self.articles()})
-
-    def article_titles(self):
-        return [article.title for article in self.articles()] or None
-
-    def contributing_authors(self):
-        authors = [article.author for article in self.articles()]
-        return [author for author in set(authors) if authors.count(author) > 2] or None
-
-    @classmethod
-    def top_publisher(cls):
-        if not cls.magazines:
-            return None
-        return max(cls.magazines, key=lambda magazine: len(magazine.articles()))
-
-
 class Article:
-    articles = []
+    all = []  # Changed from 'articles' to 'all'
 
     def __init__(self, author, magazine, title):
         if not isinstance(author, Author):
@@ -92,7 +11,7 @@ class Article:
         self._author = author
         self._magazine = magazine
         self._title = title
-        Article.articles.append(self)
+        Article.all.append(self)  # Changed from 'articles' to 'all'
 
     @property
     def title(self):
@@ -121,3 +40,84 @@ class Article:
         if not isinstance(value, Magazine):
             raise Exception("Invalid magazine instance.")
         self._magazine = value
+
+
+class Author:
+    def __init__(self, name):
+        if not isinstance(name, str) or len(name) == 0:
+            raise Exception("Name must be a non-empty string.")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Author name is immutable.")
+
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
+
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
+
+    def add_article(self, magazine, title):
+        if not isinstance(magazine, Magazine):
+            raise Exception("Invalid magazine instance.")
+        return Article(self, magazine, title)
+
+    def topic_areas(self):
+        return list({magazine.category for magazine in self.magazines()}) or None
+
+
+class Magazine:
+    all = []  # Changed from 'magazines' to 'all' for consistency
+
+    def __init__(self, name, category):
+        if not isinstance(name, str) or not (2 <= len(name) <= 16):
+            raise Exception("Name must be a string between 2 and 16 characters.")
+        if not isinstance(category, str) or len(category) == 0:
+            raise Exception("Category must be a non-empty string.")
+        self._name = name
+        self._category = category
+        Magazine.all.append(self)  # Changed from 'magazines' to 'all'
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (2 <= len(value) <= 16):
+            raise Exception("Name must be a string between 2 and 16 characters.")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise Exception("Category must be a non-empty string.")
+        self._category = value
+
+    def articles(self):
+        return [article for article in Article.all if article.magazine == self]
+
+    def contributors(self):
+        return list({article.author for article in self.articles()})
+
+    def article_titles(self):
+        return [article.title for article in self.articles()] or None
+
+    def contributing_authors(self):
+        authors = [article.author for article in self.articles()]
+        return [author for author in set(authors) if authors.count(author) > 2] or None
+
+    @classmethod
+    def top_publisher(cls):
+        if not cls.all:
+            return None
+        return max(cls.all, key=lambda magazine: len(magazine.articles()))
